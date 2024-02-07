b281b96e189506ce76ed48974c15dce0b15f5768
ahmedSadraouii <ahmed.sadraouiu@xilyor.com>
2024-02-08 00:29:12 +0100

    implementing Invoices pagination and search

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -7,7 +7,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
-      <div className={`${inter.className} antialiased`}>{children}</div>
+      <div className={`${inter.className} w-screen antialiased`}>
+        {children}
+      </div>
     </div>
   );
 }
